8efa09fcc011b669d137db95f10ab7cf70c34fbe
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-21 00:18:00 +0100

    feat: update dark mode background color
    Updated the dark mode background color in the global styles to a darker
    shade (#121212) for better contrast and readability.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 <style>
 :global{
   body {
-    --at-apply: min-h-screen bg-white text-gray-700 font-sans dark:(bg-gray-900 text-gray-200);
+    --at-apply: min-h-screen bg-white text-gray-700 font-sans dark:(bg-[#121212] text-gray-200);
   }
 }
 </style>
