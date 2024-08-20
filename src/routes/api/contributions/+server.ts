596f45a069d8c93e1d20da4ec529ba5bc2fc50a7
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-20 20:48:15 +0100

    feat(api): add contributions endpoint using Octokit
    - Added a new endpoint at `src/routes/api/contributions/+server.ts`
    - Utilizes Octokit to fetch user data and pull requests
    - Filters and formats pull requests to include only relevant information
    - Returns user details and pull requests as JSON

---
+++ src/routes/api/contributions/+server.ts
@@ -0,0 +1,33 @@
+import { json } from '@sveltejs/kit';
+import { Octokit } from 'octokit';
+import type { RequestHandler } from './$types';
+import { env } from '$env/dynamic/private';
+
+export const GET = (async () => {
+	const octokit = new Octokit({
+		auth: env.GH_TOKEN,
+	});
+	// Fetch user from token
+	const userResponse = await octokit.request('GET /user');
+	const user = {
+		name: userResponse.data.name ?? userResponse.data.login,
+		username: userResponse.data.login,
+		avatar: userResponse.data.avatar_url,
+	};
+	// Fetch pull requests from user
+	const { data } = await octokit.request('GET /search/issues', {
+		q: `type:pr+author:"${user.username}"+-user:"${user.username}"`,
+		per_page: 50,
+		page: 1,
+	});
+	const prs = data.items.filter(pr => !(pr.state === 'closed' && pr.pull_request?.merged_at == null)).map(pr => ({
+		repo: pr.repository_url.split('/').slice(-2).join('/'),
+		title: pr.title,
+		url: pr.html_url,
+		created_at: pr.created_at,
+		state: pr.pull_request?.merged_at != null ? 'merged' : pr.state,
+		number: pr.number,
+	}));
+
+	return json({ user, prs });
+}) satisfies RequestHandler;
