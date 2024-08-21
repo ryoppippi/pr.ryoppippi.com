af5cc390effe197398cbbb39c84475fd96e7b372
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-21 10:23:37 +0100

    chore: format

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,9 @@
+import type { PageServerLoad } from './$types';
 import type { Contributions } from '$lib';
 
-export async function load({ fetch }) {
+export const load: PageServerLoad = async ({ fetch }) => {
 	const res = await fetch(`/api/contributions`);
 	const { user, prs } = await res.json() as Contributions;
 
 	return { user, prs };
-}
+};
--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 declare namespace svelteHTML {
 	import type { AttributifyAttributes } from '@unocss/preset-attributify';
 
