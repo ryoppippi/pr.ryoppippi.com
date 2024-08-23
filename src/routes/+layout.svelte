5a1b9cc0f4fff626ce1182e97d654df2fbf1649e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-23 22:18:04 +0100

    feat(+layout.svelte): add motion-safe transition to body styles
    Updated the body styles to include motion-safe transition properties.
    This ensures that transitions are only applied when the user has not
    disabled animations in their system preferences.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,8 +15,7 @@
 <style>
 :global{
   body {
-    --at-apply: min-h-screen bg-white text-gray-700 font-sans dark:(bg-[#121212] text-gray-200);
-    transition: background-color 1s;
+    --at-apply: min-h-screen bg-white text-gray-700 font-sans dark:(bg-[#121212] text-gray-200) motion-safe:(transition transition-duration-1s);
   }
 }
 </style>
