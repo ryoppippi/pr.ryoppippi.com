4bc99405b8fcbab5c4157be35656c4610be7e664
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-24 01:17:06 +0100

    refactor(Head.svelte): use snippet
    Refactored meta tags in Head.svelte to use a snippet for rendering.
    This change improves code readability and maintainability by reducing
    repetition of meta tag definitions.

--- src/routes/Head.svelte
+++ src/routes/Head.svelte
@@ -9,19 +9,26 @@
 	const description = `${user.username}'s recent pull requests`;
 </script>
 
+{#snippet meta(kind: string, property: string, content: string)}
+	<meta name='{kind}:{property}' {content} />
+{/snippet}
+
 <svelte:head>
 	<title>{title}</title>
 	<meta name='description' content={description} />
+	<link href={faviconURL} rel='icon' />
+
 	<meta name='theme-color' content='#121212' media='(prefers-color-scheme: dark)' />
 	<meta name='theme-color' content='#ffffff' media='(prefers-color-scheme: light)' />
-	<meta name='twitter:title' content={title} />
-	<meta name='twitter:description' content={description} />
-	<meta name='twitter:card' content='summary' />
-	<meta name='twitter:image' content={faviconURL} />
-	<meta name='twitter:image:alt' content={title} />
-	<meta name='og:title' content={title} />
-	<meta name='og:description' content={description} />
-	<meta name='og:image' content={faviconURL} />
-	<meta name='og:image:alt' content={title} />
-	<link href={faviconURL} rel='icon' />
+
+	{@render meta('twitter', 'title', title)}
+	{@render meta('twitter', 'description', description)}
+	{@render meta('twitter', 'image', faviconURL)}
+	{@render meta('twitter', 'image:alt', title)}
+	{@render meta('twitter', 'card', 'summary')}
+
+	{@render meta('og', 'title', title)}
+	{@render meta('og', 'description', description)}
+	{@render meta('og', 'image', faviconURL)}
+	{@render meta('og', 'image:alt', title)}
 </svelte:head>
