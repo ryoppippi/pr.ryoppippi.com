41aee6e76d8713484f17fdce7fd4073763ad0453
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-12-10 15:30:53 +0900

    feat: use prefersReducedMotion from `svelte/motion` for darkmode

--- src/lib/DarkMode/transition.svelte.ts
+++ src/lib/DarkMode/transition.svelte.ts
@@ -1,31 +1,21 @@
-import { on } from 'svelte/events';
+import { prefersReducedMotion } from 'svelte/motion';
 import { browser } from '$app/environment';
 
 /**
  * check if the browser supports appearance transition
  */
 export class CheckTransitions {
-	#isReduced = $state(false);
 	#isViewTransitionAvailable = $state(false);
-	#mediaQuery: MediaQueryList | undefined = undefined;
 
-	#isAppearanceTransition = $derived(this.#isViewTransitionAvailable && !this.#isReduced);
 	constructor() {
 		if (!browser) {
 			return;
 		}
 
 		this.#isViewTransitionAvailable = document.startViewTransition != null;
-		this.#mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
-		this.#isReduced = this.#mediaQuery.matches;
-
-		on(this.#mediaQuery, 'change', (_event) => {
-			const event = _event;
-			this.#isReduced = event.matches;
-		});
 	}
 
 	get isAppearanceTransition() {
-		return this.#isAppearanceTransition;
+		return !prefersReducedMotion.current && this.#isViewTransitionAvailable;
 	}
 }
