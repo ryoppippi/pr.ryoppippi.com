a2daf46b790430c1cf83cf70d3538ce1d0c892ca
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-01-25 14:19:39 +0000

    feat: remove view-transition styles from layout because it is inclueded in svelte-fancy-darkmode
    The view-transition styles have been removed from the layout to simplify
    the CSS and improve maintainability. This change affects the z-index and
    animation properties of the view-transition elements.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,23 +17,5 @@
   body {
     --at-apply: min-h-screen bg-white text-gray-700 font-sans dark:(bg-[#121212] text-gray-200) motion-safe:(transition transition-duration-1s)
   }
-
-  ::view-transition-old(root),
-  ::view-transition-new(root) {
-    animation: none;
-    mix-blend-mode: normal;
-  }
-  ::view-transition-old(root) {
-    z-index: 1;
-  }
-  ::view-transition-new(root) {
-    z-index: 9999;
-  }
-  .dark::view-transition-old(root) {
-    z-index: 9999;
-  }
-  .dark::view-transition-new(root) {
-    z-index: 1;
-  }
 }
 </style>
