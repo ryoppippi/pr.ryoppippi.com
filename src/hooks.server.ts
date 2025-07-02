b31e612f98fb31ef4b51a96f2fd45744fb43b041
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-07-02 10:54:59 +0100

    chore: implement cache on hooks

---
+++ src/hooks.server.ts
@@ -0,0 +1,23 @@
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	if (globalThis.caches == null) {
+		return resolve(event);
+	}
+
+	const cache = await caches.open('sveltekit-cache');
+
+	const cachedResponse = await cache.match(event.request.url);
+	if (cachedResponse != null) {
+		return cachedResponse;
+	}
+	const response = await resolve(event);
+
+	const cacheMaxAgeSec = 60 * 15; // 15 minutes
+	response.headers.set('Cache-Control', `public, max-age=${cacheMaxAgeSec}, s-maxage=${cacheMaxAgeSec}`);
+
+	event.platform?.ctx.waitUntil(
+		cache.put(event.request, response.clone()), // Clone the response to avoid consuming it
+	);
+	return response;
+};
