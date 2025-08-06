cb641a0a1388951ff8387638a1592505aa200670
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-08-06 14:37:24 +0100

    feat: enable experimental async

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,12 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
+	compilerOptions: {
+		experimental: {
+			async: true,
+		},
+	},
+
 	kit: {
 		experimental: {
 			remoteFunctions: true,
